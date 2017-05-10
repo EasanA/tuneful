4011a6371dcbb8901cb2e37eb404b4331c26512f
EasanA <easan.arulanantham@gmail.com>
2017-05-10 00:48:20 +0000

    stuck on post

--- tuneful/api.py
+++ tuneful/api.py
@@ -11,12 +11,51 @@ from . import app
 from .database import session
 from .utils import upload_path
 
+song_schema = {
+    "type": "object",
+    "properties": {
+        "file": {
+            "type": "object",
+            "properties": {
+                "id": {"type": "number"}
+            },
+        },
+    },
+    "required": ["file"]
+}
 
 @app.route("/api/songs", methods=["GET"])
 @decorators.accept("application/json")
 def songs_get():
     """ Get a list of songs """
     songs = session.query(models.Song).all()
-    
+
     data = json.dumps([song.as_dictionary() for song in songs])
     return Response(data, 200, mimetype="application/json")
+    
+@app.route("/api/posts", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_post():
+    """ Add a new post """
+    data = request.json
+
+    # Check that the JSON supplied is valid
+    # If not you return a 422 Unprocessable Entity
+    try:
+        validate(data, song_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    # Add the post to the database
+    song = models.Song(file=data["file"],)
+    session.add(song)
+    session.commit()
+
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(song.as_dictionary())
+    headers = {"Location": url_for("get_songs")}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
--- tuneful/tests/api_tests.py
+++ tuneful/tests/api_tests.py
@@ -62,5 +62,39 @@ class TestAPI(unittest.TestCase):
         song2 = data[1]
         self.assertEqual(song2["file"]["name"], "song2")
         
+    def test_post_song(self):
+        """ Posting a new song """
+        file1 = models.File(name= "song1")
+        
+        session.add(file1)
+        session.commit()
+        
+        data = {
+            "file": {
+            "id": file1.id
+                }
+        }
+
+        response = self.client.post("/api/songs",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+        self.assertEqual(urlparse(response.headers.get("Location")).path,
+                         "/api/songs")
+
+        data = json.loads(response.data.decode("ascii"))
+        self.assertEqual(data["id"], file1.id)
+        self.assertEqual(data["file"]["name"], "song1")
+
+        songs = session.query(models.Song).all()
+        self.assertEqual(len(songs), 1)
+
+        song = songs[0]
+        self.assertEqual(song.file.name, "song1")
+        
 if __name__ == "__main__":
     unittest.main()
